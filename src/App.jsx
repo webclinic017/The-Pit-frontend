ca80c263f116d7c471a40e9c410e2b885ed74ae7
nicholasdegrate <nicholasdegrate@gmail.com>
2021-04-27 14:02:58 -0500

    public and private routes are working

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ChakraPovider } from '@chakra-ui/react';
-
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 // helpers
 import { PublicRoute } from './helpers/PublicRoute';
-// import { PrivateRoute } from './helpers/PrivateRoute';
+import { PrivateRoute } from './helpers/PrivateRoute';
 
 // pages
 import Home from './pages/Home';
-// import DashBoard from './pages/DashBoard';
+import DashBoard from './pages/DashBoard';
 
 export default function App() {
 	return (
-		<Router>
-			<Switch>
-				<PublicRoute restricted={false} component={Home} path="/" exact />
-				{/* <PrivateRoute component={DashBoard} path="/dashboard" exact /> */}
-			</Switch>
-		</Router>
+		<ChakraProvider>
+			<Router>
+				<Switch>
+					<PublicRoute restricted={false} component={Home} path="/" exact />
+					<PrivateRoute component={DashBoard} path="/dashboard" exact />
+				</Switch>
+			</Router>
+		</ChakraProvider>
 	);
 }
--- src/helpers/PrivateRoute.jsx
+++ src/helpers/PrivateRoute.jsx
@@ -1,9 +1,9 @@
-// import React from 'react';
-// import { Route, Redirect } from 'react-router-dom';
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
-// // utils
-// import { isLogin } from '../utils/detect-auth';
+// utils
+import { isLogin } from '../utils/detect-auth';
 
-// export const PrivateRoute = ({ component: Component, ...rest }) => {
-// 	return <Route {...rest} render={(props) => (isLogin() ? <Component {...props} /> : <Redirect to="/login" />)} />;
-// };
+export const PrivateRoute = ({ component: Component, ...rest }) => {
+	return <Route {...rest} render={(props) => (isLogin() ? <Component {...props} /> : <Redirect to="/login" />)} />;
+};
