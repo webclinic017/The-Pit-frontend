21076b99b0eed19e1810b9ac11bbf3af3c3f85ab
nicholasdegrate <nicholasdegrate@gmail.com>
2021-04-28 00:57:06 -0500

    working on dashboard

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,3 +1,15 @@
+import { Grid, Box } from '@chakra-ui/react';
+
 export default function DashBoard() {
-	return <div>hello dashboard</div>;
+	return (
+		<Grid gridTemplateColumns="5% 1fr 5%">
+			<Box h="100vh" bg="#0D1431" />
+			<Grid gridTemplateRows="5% 1fr 5%" gridTemplateColumns="1fr" h="100vh" bg="#23a729">
+				<Box w="100%" bg="#111839" />
+				<Box w="100%" bg="#0D1431" />
+				<Box w="100%" bg="#111839" />
+			</Grid>
+			<Box h="100vh" bg="#111839" />
+		</Grid>
+	);
 }
